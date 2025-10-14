dcd3511aae3f4295f85ce0b24f460d9c15575df7
Aleksandra Provorova <sashaprovora@gmail.com>
2025-10-14 03:51:05 +0300

    fix: uninstall joi validator

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,15 @@
-import path from 'path';
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import { errors } from 'celebrate';
-import productRouter from './routes/product';
-import orderRouter from './routes/order';
-import errorHandler from './middlewares/error-handler';
-import { requestLogger, errorLogger } from './middlewares/logger';
+import path from "path";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { errors } from "celebrate";
+import productRouter from "./routes/product";
+import orderRouter from "./routes/order";
+import errorHandler from "./middlewares/error-handler";
+import { requestLogger, errorLogger } from "./middlewares/logger";
 
-const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env;
+const { PORT = 3000, DB_ADDRESS = "mongodb://127.0.0.1:27017/weblarek" } =
+  process.env;
 const app = express();
 
 app.use(cors());
@@ -16,12 +17,12 @@ app.use(express.json());
 
 mongoose.connect(DB_ADDRESS);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(requestLogger);
 
-app.use('/product', productRouter);
-app.use('/order', orderRouter);
+app.use("/product", productRouter);
+app.use("/order", orderRouter);
 
 app.use(errorLogger);
 
